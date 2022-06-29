38bbb3af0f8d2573f97fda07690a323f9aa52913
aruntejamenda <aruntejamenda@gmail.com>
2022-06-30 01:31:19 +0530

    final_update_of_navigation

--- events.php
+++ events.php
@@ -13,28 +13,49 @@ include "./repeats/header.php";
         </div>
     </section>
     <section id="two">
-            <div id="two1" class="x" onclick="colorchanger(this.id);">Centerstage</div>
-            <div id="two2" class="x" onclick="colorchanger(this.id);">Departmental</div>
-            <div id="two3" class="x" onclick="colorchanger(this.id);">Talks</div>
+            <div id="two1" class="x">Centerstage</div>
+            <div id="two2">Departmental</div>
+            <div id="two3">Talks</div>
     </section>
     <script>
-        function colorchanger(id){
-            document.getElementsByClassName('x').background='unset';
-            document.getElementsByClassName('x').color='white';
-            //----
-            document.getElementById(id).style.background='#19d2a6';
-            document.getElementById(id).style.color='black';
-        }
+      $(document).ready(function(){
+        $("#two div").click(function(){
+          $(this).addClass("x").siblings().removeClass("x"); //for buttton highlighting
+          //--------------
+          $("#centerstage").removeClass("shower"); //for removing centerstage on 1st click
+          if(this.id=="two1"){
+            $("#centerstage").addClass("shower").siblings().removeClass("shower");
+          }else if(this.id=="two2"){
+            $("#departments").addClass("shower").siblings().removeClass("shower");
+          }else{
+            $("#talks").addClass("shower").siblings().removeClass("shower");
+          }
+          //--------------
+        });
+      });
     </script>
     <section id="three">
         <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
-        <div id="departments" style="display: none;">
-            <script>
-                var patch = '<div class="card"><div id="cardtop"><img src="images/card_back.jpg" alt="card_back"></div><div id="cardbottom"><span id="text">Electronic and Communication</span><span id="logo"><span style="color:#19d2a6;">></span>></span></div></div>';
-                for(var i=0;i<10;i++){document.write(patch);}
-            </script>
+        <div id="centerstage" class="shower">centerstage</div>
+        <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+        <div id="departments" class="">
+            <div id="dfilters">
+              <button id="all" class="x" data-filter="all">ALL</button>
+              <button id="cse" data-filter="cse">Cse</button>
+              <button id="ece" data-filter="ece">Ece</button>
+              <button id="chem" data-filter="chem">Chem</button>
+              <button id="civil" data-filter="civil">Civil</button>
+              <button id="mech" data-filter="mech">Mech</button>
+            </div>
+            <div id="cards_container">
+              <script>var patch = '<div class="card cse"><div id="cardtop"><img src="images/card_back.jpg" alt="card_back"></div><div id="cardbottom"><span id="text">cse</span><span id="logo"><span style="color:#19d2a6;">></span>></span></div></div>';for(var i=0;i<3;i++){document.write(patch);}</script>
+              <script>var patch = '<div class="card ece"><div id="cardtop"><img src="images/card_back.jpg" alt="card_back"></div><div id="cardbottom"><span id="text">Electronic and Communication</span><span id="logo"><span style="color:#19d2a6;">></span>></span></div></div>';for(var i=0;i<3;i++){document.write(patch);}</script>
+              <script>var patch = '<div class="card chem"><div id="cardtop"><img src="images/card_back.jpg" alt="card_back"></div><div id="cardbottom"><span id="text">chem</span><span id="logo"><span style="color:#19d2a6;">></span>></span></div></div>';for(var i=0;i<3;i++){document.write(patch);}</script>
+              <script>var patch = '<div class="card civil"><div id="cardtop"><img src="images/card_back.jpg" alt="card_back"></div><div id="cardbottom"><span id="text">civil</span><span id="logo"><span style="color:#19d2a6;">></span>></span></div></div>';for(var i=0;i<3;i++){document.write(patch);}</script>
+              <script>var patch = '<div class="card mech"><div id="cardtop"><img src="images/card_back.jpg" alt="card_back"></div><div id="cardbottom"><span id="text">mech</span><span id="logo"><span style="color:#19d2a6;">></span>></span></div></div>';for(var i=0;i<3;i++){document.write(patch);}</script>
+            </div>
             <!------------CARD--------------->
-            <!-- <div class="card">
+            <!-- <div class="card cse">
                 <div id="cardtop">
                     <img src="images/card_back.jpg" alt="card_back">
                 </div>
@@ -42,11 +63,23 @@ include "./repeats/header.php";
                     <span id="text">Electronic and Communication</span>
                     <span id="logo"><span style="color:#19d2a6;">></span>></span>
                 </div>
-            </div>-->
+            </div> -->
             <!------------CARD--------------->
+            <script>
+              $("#dfilters button").click(function(){
+                var value = $(this).attr("data-filter");
+                $(this).addClass("x").siblings().removeClass("x");
+                if(this.id == "all"){
+                  $("#cards_container .card").show('1000');
+                }else{
+                  $("#cards_container .card").not('.'+value).hide('1000');
+                  $("#cards_container .card").filter('.'+value).show('1000');
+                }
+              });
+            </script>
         </div>
         <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
-        <div id="talks">
+        <div id="talks" class="">
             <span id="thead">Talks</span>
             <div id="guests">
                 <script>
@@ -73,8 +106,6 @@ include "./repeats/header.php";
                 <!------------GUEST_CARD--------------->
             </div>
         </div>
-        <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
-        <div id="filter3"></div>
         <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
     </section>
 </body>
