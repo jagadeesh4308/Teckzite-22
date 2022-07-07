98ee33f6fed78523155418dde3f2c875307e1511
jagadeesh <jagadeesh4308@gmail.com>
2022-07-08 01:58:55 +0530

    Profile page backend integration

--- profile.php
+++ profile.php
@@ -2,18 +2,32 @@
 
     include "repeats/header.php";
     include "includes/connect.php";
-
+    include "./includes/usrGetBack.php";
 ?>
+
+
 <section id="as">
     <div id="as1">
         <h1>Profile</h1>
     </div>
+    <a href="logout.php">logout</a>
+    <br>
     <div id="as2">
-        <img src="images/Me.jpg" alt="profile_pic">
-        <div id="pinfo">
-            <h1 id="tzid">TZ220011</h1>
-            <h1 id="uname">USERRRR KHAN</h1>
-            <h1 id="umail">Userr_kanna@outlook.com</h1>
+
+    <?php 
+            
+        $useremail = $_SESSION['user_email_address'];
+        $username = $_SESSION['user_first_name'].' '.$_SESSION['user_last_name'];
+        $tzID = $_SESSION['tzID'];
+        $profilePic = $_SESSION['user_image'];
+
+        echo "<img src='$profilePic' alt='profile_pic'>
+            <div id='pinfo'>
+            
+                <h1 id='tzid'>$tzID</h1>
+                <h1 id='uname'>$username</h1>
+                <h1 id='umail'>$useremail</h1>";
+            ?>
         </div>
     </div>
 </section>
@@ -26,4 +40,3 @@
         <div id="eh">REGISTERED<div id="ehi">WORKSHOPS</div></div>
     </div>
 </section>
-<a href="logout.php">logout</a>
