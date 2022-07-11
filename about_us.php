dce0f51600bf8eef11e916d61b09414100f09ba6
aruntejamenda <aruntejamenda@gmail.com>
2022-07-12 01:06:13 +0530

    about_us_page

---
+++ about_us.php
@@ -0,0 +1,10 @@
+<?php
+    include "repeats/header.php";
+?>
+<section id="main_container" class="about_us_container">
+    <h1>ABOUT US</h1>
+    
+</section>
+<?php
+    include "repeats/footer.php";
+?>
