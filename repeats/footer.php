de59f5149d84529c2fdf0d15a50a0667b93640b3
aruntejamenda <aruntejamenda@gmail.com>
2022-07-11 23:33:23 +0530

    final_footer_commit

--- repeats/footer.php
+++ repeats/footer.php
@@ -1,13 +1,17 @@
 
   <div id="fl">
-      <div id="flb">b</div>
+      <div id="flb">
+        <a href="https://www.youtube.com/channel/UCRfeF0qcPwFCWw6y5GsML7g"><i class="fa fa-youtube"></i></a>
+        <a href="https://www.facebook.com/teckzite"><i class="fa fa-facebook"></i></a>
+        <a href="https://twitter.com/teckzite"><i class="fa fa-twitter"></i></a>
+        <a href="https://www.instagram.com/teckzite_rguktn/"><i class="fa fa-instagram"></i></a>
+      </div>
   </div>
   <div id="fm">
-    <span></span>
-    <span>⨁⨀⨂⨸⨷Θθ</span>
+    <span><strong>Copyrights By Teckzite.org 2022<br>All Rights Reserved.</strong></span>
   </div>
   <div id="fr">
-    <div id="frb">b</div>
+    <div id="frb"><h1><a href="#">Notifications</a></h1></div>
   </div>
 <body>
 </html>
